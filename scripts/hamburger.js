a2012ffa258d3229a0e8a7cef74ffd63b6c781c2
jonahhwest0408 <jonah.hwest0408@gmail.com>
2024-05-02 14:10:17 -0700

    Hamburger menu bugs and fixes

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -1,9 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
     var hamburgerButton = document.getElementById('hamburger-button');
+    var closeButton = document.getElementById('close-button');
     var mainNav = document.getElementById('main-nav');
 
     // Toggle visibility of mainNav when hamburgerButton is clicked
     hamburgerButton.addEventListener('click', function () {
         mainNav.classList.toggle('open');
+        hamburgerButton.style.display = 'none'; // Hide hamburger button
+        closeButton.style.display = 'block'; // Show close button
+    });
+
+    closeButton.addEventListener('click', function () {
+        mainNav.classList.remove('open');
+        hamburgerButton.style.display = 'block'; // Show hamburger button
+        closeButton.style.display = 'none'; // Hide close button
     });
 });
