5c6ac69cb29918c117f79be7ef1f0d35fabbb2a2
jonahhwest0408 <jonah.hwest0408@gmail.com>
2024-05-08 12:14:18 -0700

    Modal bug pt2

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -21,23 +21,17 @@ function updatePageVisits() {
 
 window.onload = updatePageVisits;
 
-// Check if the user has visited the site before
 if (!localStorage.lastVisitDate) {
-    // If not, set the current date as the last visit date
     localStorage.lastVisitDate = new Date().toLocaleDateString();
   }
   
-  // Get the last visit date from local storage
   const lastVisitDate = localStorage.lastVisitDate;
   
-  // Display the last visit date in the modal
   document.getElementById('last-visit-date').textContent = lastVisitDate;
   
-  // Show the modal to the user
   const modal = document.getElementById('myModal');
   modal.style.display = 'block';
   
-  // Add an event listener to close the modal when the user clicks on the close button
   const closeBtn = document.getElementsByClassName('close')[0];
   closeBtn.addEventListener('click', () => {
     modal.style.display = 'none';
