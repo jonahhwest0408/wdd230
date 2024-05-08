8e3bcf7125b4071d15ac96cc40bb790d4f11e10f
jonahhwest0408 <jonah.hwest0408@gmail.com>
2024-05-08 11:56:17 -0700

    Sidebar addition - New Discover page

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -20,3 +20,37 @@ function updatePageVisits() {
 }
 
 window.onload = updatePageVisits;
+
+// last visited modal
+var modal = document.getElementById("myModal");
+
+var span = document.getElementsByClassName("close")[0];
+
+var lastVisit = localStorage.getItem("lastVisit");
+
+if (lastVisit) {
+  var lastVisitDate = new Date(lastVisit);
+  var currentDate = new Date();
+  var timeDiff = Math.abs(currentDate.getTime() - lastVisitDate.getTime());
+  var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
+
+  var message = "";
+  if (diffDays == 1) {
+    message = "You last visited 1 day ago.";
+  } else {
+    message = "You last visited " + diffDays + " days ago.";
+  }
+
+  document.getElementById("last-visit-message").innerText = message;
+  document.getElementById("last-visit-date").innerText = lastVisitDate.toDateString();
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
